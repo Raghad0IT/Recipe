e39f68fedaaa08e29fa3d307740f948844d5212a
Raghad0IT <raghadtuwaiq@gmail.com>
2024-10-27 17:22:41 +0300

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Raghad on 24/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecipeTests/RecipeTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeTests.swift
+//  RecipeTests
+//
+//  Created by Raghad on 24/04/1446 AH.
+//
+
+import Testing
+@testable import Recipe
+
+struct RecipeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
